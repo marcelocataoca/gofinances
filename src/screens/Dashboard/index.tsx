79777d646797e77ccffe1274469edcc119bc9156
marcelocataoca <marcelokenjicatao@gmail.com>
2021-06-19 17:46:51 -0300

    feat: add responsive fontsize, user greeting

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Container, Text } from "./styles";
+import { Text } from "react-native";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/19317136?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Kenji</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
