e47d5da1526cb878032118039c7a6a15a6388196
marcelocataoca <marcelokenjicatao@gmail.com>
2021-06-21 09:24:08 -0300

    feat: add icon lib vector-icons expo

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Text } from "react-native";
 import {
   Container,
   Header,
@@ -9,6 +8,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon,
 } from "./styles";
 
 export function Dashboard() {
@@ -27,6 +27,7 @@ export function Dashboard() {
               <UserName>Kenji</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
