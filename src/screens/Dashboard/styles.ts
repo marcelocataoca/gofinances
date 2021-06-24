57cfd21602965c06184d888268b6b8fcd7f68141
marcelocataoca <marcelokenjicatao@gmail.com>
2021-06-23 23:37:46 -0300

    feat: set highlight card position, header

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -11,13 +11,14 @@ export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(42)}px;
     background-color: ${({theme}) => theme.colors.primary};
-    align-items: center;
+    align-items: flex-start;
     justify-content: flex-start;
     flex-direction: row;
 `
 export const UserWrapper = styled.View`
     width: 100%;    
     padding: 0 24px;
+    margin-top: ${RFValue(28)}px;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -53,4 +54,8 @@ export const HighlightCards = styled.ScrollView.attrs({
     horizontal: true,
     showsHorizontalScrollIndicator: false,
     contentContainerStyle:{ paddingHorizontal: 24 }
-})``;
+})`
+    width: 100%;
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
