bf5e13b4729afce62e438ae5c63218b9a539a942
marcelocataoca <marcelokenjicatao@gmail.com>
2021-07-26 10:25:13 -0300

    category select

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 import {
     Container,
     Header,
@@ -41,6 +42,7 @@ export function Register() {
                             isActive={transactionType === "down"}
                         />
                     </TransactionsTypes>
+                    <CategorySelect title="Categoria" />
                 </Fields>
                 <Button title="Enviar" />
             </Form>
--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -29,6 +29,8 @@ export const Form = styled.View`
 export const Fields = styled.View``; 
 
 export const TransactionsTypes = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 8px;
+    margin-bottom: 16px;
 `
