a6843df732bd22b4f721af9e1ecdd856685f8dba
marcelocataoca <marcelokenjicatao@gmail.com>
2021-07-27 09:56:18 -0300

    add category screen

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
-import { CategorySelect } from "../../components/Form/CategorySelect";
 import {
     Container,
     Header,
@@ -11,12 +10,22 @@ import {
     Fields,
     TransactionsTypes,
 } from "./styles";
+import { CategorySelect } from "../CategorySelect";
 
 export function Register() {
     const [transactionType, setTransactionType] = useState("");
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
     function handleTransactionsTypeSelect(type: "up" | "down") {
         setTransactionType(type);
+    }
+
+    function handleOpenSelectCategoryModal() {
+        setCategoryModalOpen(true);
+    }
+
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false);
     }
 
     return (
