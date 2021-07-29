ada6da92488c4ead621f32fbf6fbe56a548c272e
marcelocataoca <marcelokenjicatao@gmail.com>
2021-07-29 09:14:43 -0300

    add yup validation

--- src/components/Form/InputForm/index.tsx
+++ src/components/Form/InputForm/index.tsx
@@ -3,14 +3,15 @@ import { TextInputProps } from "react-native";
 import { Control, Controller } from "react-hook-form";
 
 import { Input } from "../Input";
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 
 interface Props extends TextInputProps {
   control: Control;
   name: string;
+  error: string;
 }
 //uso do Controller para altera o state somente no submit e não em todo momento
-export function InputForm({ control, name, ...rest }: Props) {
+export function InputForm({ control, name, error, ...rest }: Props) {
   return (
     <Container>
       <Controller
@@ -20,6 +21,7 @@ export function InputForm({ control, name, ...rest }: Props) {
         )}
         name={name}
       ></Controller>
+      {error && <Error>{error}</Error>}
     </Container>
   );
 }
