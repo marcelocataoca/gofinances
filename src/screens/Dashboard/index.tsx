4b2057b616af3ca4451165f45064416c0a5b9ec8
marcelocataoca <marcelokenjicatao@gmail.com>
2021-09-12 10:22:23 -0300

    fix list transaction

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { HighlightCard } from "../../components/HighlightCard";
 import {
     TransactionCard,
@@ -26,41 +28,49 @@ export interface DataListProps extends TransactionCardProps {
 }
 
 export function Dashboard() {
-    const data: DataListProps[] = [
-        {
-            id: "1",
-            title: "Desenvolvimento de site",
-            amount: "R$ 12.000,00",
-            type: "positive",
-            category: {
-                name: "Vendas",
-                icon: "dollar-sign",
-            },
-            date: "13/04/2021",
-        },
-        {
-            id: "2",
-            title: "Hamburgueria Pizzy",
-            amount: "R$ 59,00",
-            type: "negative",
-            category: {
-                name: "Alimentação",
-                icon: "coffee",
-            },
-            date: "10/04/2021",
-        },
-        {
-            id: "3",
-            title: "Desenvolvimento de site",
-            amount: "R$ 1.200,00",
-            type: "negative",
-            category: {
-                name: "Casa",
-                icon: "shopping-bag",
-            },
-            date: "10/04/2021",
-        },
-    ];
+    const [data, setData] = useState<DataListProps[]>([]);
+
+    async function loadTransactions() {
+        const dataKey = "@gofinances:transactions";
+        const response = await AsyncStorage.getItem(dataKey);
+        const transactions = response ? JSON.parse(response) : [];
+        console.log(transactions);
+
+        const transactionsFormatted: DataListProps[] = transactions.map(
+            (item: DataListProps) => {
+                const amount = Number(item.amount).toLocaleString("pt-Br", {
+                    style: "currency",
+                    currency: "BRL",
+                });
+
+                const date = Intl.DateTimeFormat("pt-BR", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "2-digit",
+                }).format(new Date(item.date));
+
+                return {
+                    id: item.id,
+                    name: item.name,
+                    amount,
+                    type: item.type,
+                    category: item.category,
+                    date,
+                };
+            }
+        );
+        setData(transactions);
+    }
+
+    useEffect(() => {
+        loadTransactions();
+    }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            loadTransactions();
+        }, [])
+    );
 
     return (
         <Container>
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -4,6 +4,8 @@ import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
+import uuid from "react-native-uuid";
 import { InputForm } from "../../components/Form/InputForm";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
@@ -23,6 +25,10 @@ interface FormData {
     amount: string;
 }
 
+type NavigationProps = {
+    navigate: (screen: string) => void;
+};
+
 const shema = Yup.object().shape({
     name: Yup.string().required("Nome é obrigatório"),
     amount: Yup.number()
@@ -33,17 +39,19 @@ const shema = Yup.object().shape({
 export function Register() {
     const [transactionType, setTransactionType] = useState("");
     const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-    const dataKey = "@gofinances:transactions";
 
     const [category, setCategory] = useState({
         key: "category",
         name: "Categoria",
     });
+    const navigation = useNavigation<NavigationProps>();
+
     // formState -> captura os erros que o yup encontrou
     //desestruturação do useForm
     const {
         control, //regisrar os inputs do form
         handleSubmit,
+        reset,
         formState: { errors },
     } = useForm({
         resolver: yupResolver(shema),
@@ -68,18 +76,30 @@ export function Register() {
             return Alert.alert("Selecione a categoria");
 
         const newTransaction = {
+            id: String(uuid.v4()), //v4 devolve um hash
             name: form.name,
             amount: form.amount,
             transactionType,
             category: category.key,
+            date: new Date(),
         };
 
         try {
+            const dataKey = "@gofinances:transactions";
             const data = await AsyncStorage.getItem(dataKey); //recupera os dados do storage
             const currentData = data ? JSON.parse(data) : []; //pega os dados da nova transct
 
-            const dataFormatted = { ...currentData, newTransaction };
+            const dataFormatted = [...currentData, newTransaction];
             await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+            reset();
+            setTransactionType("");
+            setCategory({
+                key: "category",
+                name: "Categoria",
+            });
+
+            navigation.navigate("Listagem");
         } catch (error) {
             console.log(error);
             Alert.alert("Não foi possivel alterar");
@@ -87,6 +107,7 @@ export function Register() {
     }
 
     useEffect(() => {
+        const dataKey = "@gofinances:transactions";
         async function loadData() {
             //useEffect não é asincrono
             const data = await AsyncStorage.getItem(dataKey);
